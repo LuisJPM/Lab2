fe0737bd42ddca2fada2aab8b4a5ac816225cc26
unknown <lperez-2020066@kinal.edu.gt>
2025-02-17 23:07:48 -0600

    carga de auth

---
+++ auth/auth.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { registro, login } from "./auth.controller.js";
+import { registerValidator, loginValidator } from "../middlewares/validators.js";
+import { deleteFileOnError } from "../middlewares/delete-file-on-error.js";
+
+const router = Router();
+
+router.post("/registro", registerValidator, deleteFileOnError, registro);
+router.post("/login", loginValidator, login);
+
+export default router;
